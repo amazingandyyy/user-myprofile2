94dff99dcfc76c5e7cb369810e08aaed6e859839
amazingandyyy <amazingandyyy@gmail.com>
2016-05-10 09:59:25 -0700

    change to token
    change to token

--- models/user.js
+++ models/user.js
@@ -31,14 +31,21 @@ var userSchema = new mongoose.Schema({
     instagram: {
         type: String
     },
-    picture:{
+    picture: {
         type: String
     }
 });
 
 // IT'S MIDDLEWARE!!
 userSchema.statics.isLoggedIn = function(req, res, next) {
-    var token = req.cookies.accessToken;
+    // var token = req.cookies.accessToken;
+    if (!req.header('Authorization')) {
+        return res.status(401).send({
+            message: 'Please make sure your request has an Authorization header'
+        });
+    }
+    var token = req.header('Authorization').split(' ')[1];
+
 
     jwt.verify(token, JWT_SECRET, (err, payload) => {
         if (err) return res.status(401).send({
@@ -50,7 +57,7 @@ userSchema.statics.isLoggedIn = function(req, res, next) {
             .exec((err, user) => {
                 if (err || !user) {
                     // var token = localStorage.satellizer_token;
-                    return res.clearCookie('accessToken').status(400).send(err || {
+                    return res.status(400).send(err || {
                         error: 'User not found.'
                     });
                 }
@@ -99,13 +106,17 @@ userSchema.statics.authenticate = function(userObj, cb) {
         console.log("LOG", dbUser);
 
         bcrypt.compare(userObj.password, dbUser.password, (err, isGood) => {
-            if (err || !isGood) return cb(err || { error: 'Login failed. Email or password incorrect.'});
+            if (err || !isGood) return cb(err || {
+                error: 'Login failed. Email or password incorrect.'
+            });
             console.log(isGood);
             var token = dbUser.makeToken();
 
             cb(null, token);
         });
-    }).select({password: true});
+    }).select({
+        password: true
+    });
 };
 
 userSchema.methods.makeToken = function() {
