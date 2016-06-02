a9719ceb0ba27a5b7b5550637194149691ec0516
Amazingandyyy <amazingandyyy@gmail.com>
2016-06-02 00:28:00 -0700

    add MONGODB_URI
    add MONGODB_URI

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
 
-const MONGOURL = 'mongodb://localhost/user-myprofile2';
+const MONGOURL = process.env.MONGODB_URI || 'mongodb://localhost/user-myprofile2';
 
 mongoose.connect(MONGOURL, err => {
   console.log(err || `MongoDB connected to ${MONGOURL}`);
